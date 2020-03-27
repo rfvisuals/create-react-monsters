bcf5dab35885d75ad831eda7a2b877cc97fd1156
worldpeer <contact@worldpeer.com>
2020-03-27 13:19:30 -0700

    react-app first commit

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello NewUser'
+    };
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({string: "Hello Player One"})}>Change Property</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
